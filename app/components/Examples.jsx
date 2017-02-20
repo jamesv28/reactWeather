ec50a4ebfbc2f30e1271f7fe5c02c502be1a06ba
James Volmert <james.volmert@gmail.com>
2017-02-20 13:19:07 -0700

    added foundation and jquery

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,15 @@
 var React = require('react');
 
-var Examples = React.createClass({
-    render: function () {
-        return (
-            <h2>Example Component</h2>
-        )
-    }
-}); // end of example
-
+// var Examples = React.createClass({
+//     render: function () {
+//         return (
+//             <h2>Example Component</h2>
+//         )
+//     }
+// }); // end of example
+var Examples = (props) => {
+    return (
+        <h3>Abo0ut Component</h3>
+    )
+};
 module.exports = Examples;
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,11 +1,9 @@
 var React = require('react');
 
-var WeatherMessage =- React.createClass({
-    render: function () {
-        return(
-            <h3>It is 40 in Tacoma</h3>
-        )
-    }
-}); // end of WeatherMessage component
+var WeatherMessage = ({temp, location}) => {
+    return (
+        <h3>It's {temp} degrees in {location}</h3>
+    )
+};
 
 module.exports = WeatherMessage;
